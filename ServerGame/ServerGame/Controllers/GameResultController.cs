21b4f85010f5e1b1e6c80f0b3c27cbc94aff385b
Gustavo <gmpinatti@gmail.com>
2021-01-18 03:28:20 -0300

    Controllers

---
+++ ServerGame/ServerGame/Controllers/GameResultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ServerGame.Models;
+using ServerGame.Services;
+
+namespace ServerGame.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameResultController : ControllerBase
+    {
+
+        private GameService service;
+
+        public GameResultController()
+        {
+            service = new GameService();
+        }
+
+            
+        [HttpPost]
+        public ActionResult RegisterGameResult(GameResultModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return StatusCode(400, "The result is not valid");
+
+                service.Save(model);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Error to save game result");
+            }
+            
+        }
+
+    }
+}
---
+++ ServerGame/ServerGame/Controllers/LeaderboardController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerGame.Models;
+using ServerGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerGame.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LeaderboardController : ControllerBase
+    {
+        private LeaderboardService service;
+
+        public LeaderboardController() 
+        {
+            service = new LeaderboardService();
+        }
+
+        [HttpPost]
+        public ActionResult<List<LeaderboardViewModel>> GetLeaderboard(long gameId)
+        {
+            try{
+                var leaderboardsGame = service.GetLeaderboardsGame(gameId);
+                if (leaderboardsGame == null)
+                    return StatusCode(404, "Game not found");
+                return leaderboardsGame;
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Error to get leaderboards");
+            }
+        }
+
+    }
+}
