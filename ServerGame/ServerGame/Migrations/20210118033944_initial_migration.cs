9dc87490a6717b842da3f0ecb7c8cbe0247314dc
Gustavo <gmpinatti@gmail.com>
2021-01-18 03:28:40 -0300

    Migrations

---
+++ ServerGame/ServerGame/Migrations/20210118033944_initial_migration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServerGame.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameResult",
+                columns: table => new
+                {
+                    PlayerId = table.Column<long>(type: "bigint", nullable: false),
+                    GameId = table.Column<long>(type: "bigint", nullable: false),
+                    WinPoints = table.Column<long>(type: "bigint", nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameResult", x => new { x.PlayerId, x.GameId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameResult");
+        }
+    }
+}
