6c3305440e4778064a708a2add61673a467a5670
Gustavo <gmpinatti@gmail.com>
2021-01-18 03:24:08 -0300

    Create Models

---
+++ ServerGame/ServerGame/Models/GameResultModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerGame.Models
+{
+    public class GameResultModel
+    {
+        [Required]
+        public long PlayerId { get; set; }
+        [Required]
+        public long GameId { get; set; }
+        [Required]
+        public long WinPoints { get; set; }
+        [Required]
+        public DateTime TimeStamp { get; set; }
+    }
+}
