8fcbfefafb8c8e403b2358d3d8465b77e2189293
Gustavo <gmpinatti@gmail.com>
2021-01-18 03:27:46 -0300

    Services

---
+++ ServerGame/ServerGame/Services/GameService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ServerGame.DataBase;
+using ServerGame.Models;
+
+namespace ServerGame.Services
+{
+    public class GameService
+    {
+
+        private MemoryDBGame memoryDB;
+
+        public GameService()
+        {
+            memoryDB = MemoryDBGame.Instance;
+        }
+
+        public void Save(GameResultModel model)
+        {
+            memoryDB.AddGameResult(model);
+        }
+    }
+}
---
+++ ServerGame/ServerGame/Services/LeaderboardService.cs
@@ -0,0 +1,25 @@
+﻿using ServerGame.DataBase;
+using ServerGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerGame.Services
+{
+    public class LeaderboardService
+    {
+
+        public MemoryDBLeaderboard memoryDB;
+        
+        public LeaderboardService()
+        {
+            memoryDB = MemoryDBLeaderboard.Instance;
+        }
+
+        public List<LeaderboardViewModel> GetLeaderboardsGame(long gameId)
+        {
+            return memoryDB.GetLeaderboards(gameId)?.ConvertAll(x => (LeaderboardViewModel) x);
+        }
+    }
+}
